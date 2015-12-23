2371a40a9e3be900235d220ee406e098a458de90
Francisco Mota <francisco.mota@movvo.com>
2015-12-23 14:10:50 +0000

    Fixing type error in pkg/build.ml
    Changed `Ocamlbuild to (`Ocamlbuild []).

--- pkg/build.ml
+++ pkg/build.ml
@@ -3,7 +3,7 @@
 #use "topkg.ml";;
 
 let () =
-  Pkg.describe "ptime" ~builder:`OCamlbuild [
+  Pkg.describe "ptime" ~builder:(`OCamlbuild []) [
     Pkg.lib "pkg/META";
     Pkg.lib ~exts:Exts.module_library "src/ptime";
     Pkg.lib "src/ptime_top_init.ml";
