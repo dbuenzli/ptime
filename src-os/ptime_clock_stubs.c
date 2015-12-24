e88ef20b6f35c0c02c28e4b7a1bc87565341aea3
Daniel Bünzli <daniel.buenzli@erratique.ch>
2015-12-24 19:03:02 +0000

    Make sure to return valid Ptime values from C stubs.

--- src-os/ptime_clock_stubs.c
+++ src-os/ptime_clock_stubs.c
@@ -10,8 +10,12 @@
 #include <caml/fail.h>
 
 #define Val_none Val_int(0)
+#define OCAML_PTIME_DAY_MAX 2932896 // See Ptime.max
+#define OCAML_PTIME_RAISE_SYS_ERROR(ERR)                               \
+  do { caml_raise_sys_error (caml_copy_string("Ptime_clock: " ERR)); } \
+  while (0)
 
-/* Detect platform */
+/* Detect platforms and call in their includes */
 
 #if defined(__APPLE__) && defined(__MACH__)
   #define OCAML_PTIME_DARWIN
@@ -30,6 +34,7 @@
   #define OCAML_PTIME_UNSUPPORTED
 #endif
 
+
 /* Clock now */
 
 #if defined(OCAML_PTIME_POSIX)
@@ -41,12 +46,27 @@ CAMLprim value ocaml_ptime_clock_now_d_ps (value unit)
   struct timespec now;
 
   if (clock_gettime (CLOCK_REALTIME, &now))
-    caml_raise_sys_error (caml_copy_string
-                          ("Ptime_clock: can't determine current time"));
+    OCAML_PTIME_RAISE_SYS_ERROR ("can't determine current time");
+
+  /* Make sure to return valid Ptime.t values. */
+
+  /* We only handle valid timespec structs as per POSIX def (§2.8.5 in 2013) */
+if (now.tv_nsec < 0 || now.tv_nsec > 999999999)
+    OCAML_PTIME_RAISE_SYS_ERROR ("invalid tv_nsec in timespec");
+
+  /* To make it easier, we do not lie, this can't possibly be now.
+     See e.g. Ptime.Span.of_int_s if this is a problem. */
+  if (now.tv_sec < 0)
+    OCAML_PTIME_RAISE_SYS_ERROR ("negative tv_sec in timespec");
+
+  int d = now.tv_sec / 86400;
+  if (d > OCAML_PTIME_DAY_MAX)
+    OCAML_PTIME_RAISE_SYS_ERROR ("can't represent timespec in Ptime.t");
 
   pair = caml_alloc (2, 0);
-  Store_field (pair, 0, Val_int (now.tv_sec / 86400));
+  Store_field (pair, 0, Val_int (d));
   Store_field (pair, 1,
+               /* Given the above checks, in the right range for Ptime */
                caml_copy_int64 ((now.tv_sec % 86400) * 1000000000000L +
                                 (now.tv_nsec * 1000L)));
   CAMLreturn (pair);
@@ -62,9 +82,25 @@ CAMLprim value ocaml_ptime_clock_now_d_ps (value unit)
 
   gettimeofday(&now, NULL);
 
+  /* Make sure to return valid Ptime.t values. */
+
+  /* We only handle reasonable timevals (not specified in POSIX it seems) */
+  if (now.tv_usec < 0 || now.tv_usec > 999999)
+    OCAML_PTIME_RAISE_SYS_ERROR ("unreasonable tv_usec in timeval");
+
+  /* To make it easier, we do not lie, this can't possibly be now.
+     See e.g. Ptime.Span.of_int_s if this is a problem. */
+  if (now.tv_sec < 0)
+    OCAML_PTIME_RAISE_SYS_ERROR ("negative tv_sec in timeval");
+
+  int d = now.tv_sec / 86400;
+  if (d > OCAML_PTIME_DAY_MAX)
+    OCAML_PTIME_RAISE_SYS_ERROR ("can't represent timeval in Ptime.t");
+
   pair = caml_alloc (2, 0);
-  Store_field (pair, 0, Val_int (now.tv_sec / 86400));
+  Store_field (pair, 0, Val_int (d));
   Store_field (pair, 1,
+               /* Given the above checks, in the right range for Ptime */
                caml_copy_int64 ((now.tv_sec % 86400) * 1000000000000L +
                                 (now.tv_usec * 1000000L)));
   CAMLreturn (pair);
@@ -74,11 +110,11 @@ CAMLprim value ocaml_ptime_clock_now_d_ps (value unit)
 
 CAMLprim value ocaml_ptime_clock_now_d_ps (value unit)
 {
-  caml_raise_sys_error (caml_copy_string
-                        ("Ptime_clock: unsupported platform"));
+  OCAML_PTIME_RAISE_SYS_ERROR ("unsupported platform");
 }
 
 #endif
+
 
 /* Clock period */
 
@@ -89,12 +125,23 @@ CAMLprim value ocaml_ptime_clock_period_d_ps (value unit)
   CAMLparam1 (unit);
   CAMLlocal2 (some, pair);
   struct timespec res;
+
   if (clock_getres (CLOCK_REALTIME, &res)) CAMLreturn (Val_none);
+
+  /* Make sure to return valid Ptime.Span.t values. */
+
+  /* We only handle valid timespec structs as per POSIX def (§2.8.5 in 2013) */
+  if (res.tv_nsec < 0 || res.tv_nsec > 999999999) CAMLreturn (Val_none);
+
+  /* Negative periods are dubious */
+  if (res.tv_sec < 0) CAMLreturn (Val_none);
+
   some = caml_alloc (1, 0);
   pair = caml_alloc (2, 0);
   Store_field (some, 0, pair);
   Store_field (pair, 0, Val_int (res.tv_sec / 86400));
   Store_field (pair, 1,
+               /* Given the above checks, in the right range for Ptime */
                caml_copy_int64 ((res.tv_sec % 86400) * 1000000000000L +
                                 (res.tv_nsec * 1000L)));
   CAMLreturn (some);
