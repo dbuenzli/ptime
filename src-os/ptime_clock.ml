9b660b9a7e7cb1c6a3150b376acfc561519ddc99
Daniel Bünzli <daniel.buenzli@erratique.ch>
2015-12-24 22:24:52 +0100

    Reorder things a bit in ptime.clock.os.

--- src-os/ptime_clock.ml
+++ src-os/ptime_clock.ml
@@ -15,19 +15,19 @@ external ptime_clock_period_d_ps : unit -> (int * int64) option =
 external ptime_clock_current_tz_offset_s : unit -> int option =
   "ocaml_ptime_clock_current_tz_offset_s"
 
-(* Raw interface *)
-
-let now_d_ps = ptime_clock_now_d_ps
-let period_d_ps = ptime_clock_period_d_ps
-
 (* POSIX clock *)
 
-let now () = Ptime.unsafe_of_d_ps (now_d_ps ())
+let now () = Ptime.unsafe_of_d_ps (ptime_clock_now_d_ps ())
 let period () = Ptime.Span.unsafe_of_d_ps_option (ptime_clock_period_d_ps ())
 
 (* System time zone offset *)
 
 let current_tz_offset_s = ptime_clock_current_tz_offset_s
+
+(* Raw interface *)
+
+let now_d_ps = ptime_clock_now_d_ps
+let period_d_ps = ptime_clock_period_d_ps
 
 (*---------------------------------------------------------------------------
    Copyright (c) 2015 Daniel C. Bünzli.
