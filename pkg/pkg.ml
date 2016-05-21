051471445edf655ace4f2692cc4ef8e95bad61ca
Daniel Bünzli <daniel.buenzli@erratique.ch>
2016-05-21 17:50:07 +0200

    pkg.ml: use Pkg.clib.

--- pkg/pkg.ml
+++ pkg/pkg.ml
@@ -12,8 +12,7 @@ let () =
        Pkg.mllib ~api:[] "src/ptime_top.mllib";
        Pkg.lib "src/ptime_top_init.ml";
        Pkg.mllib "src-os/ptime_clock.mllib" ~dst_dir:"os/";
-       Pkg.lib ~exts:Exts.c_library "src-os/libptime_clock_stubs" ~dst:"os/";
-       Pkg.stublibs ~exts:Exts.c_dll_library "src-os/dllptime_clock_stubs";
+       Pkg.clib "src-os/libptime_clock_stubs.clib" ~lib_dst_dir:"os/";
        Pkg.mllib ~api:[] "src-os/ptime_clock_top.mllib" ~dst_dir:"os/";
        Pkg.mllib ~cond:jsoo "src-jsoo/ptime_clock.mllib" ~dst_dir:"jsoo/";
        Pkg.doc "test-os/min_clock_os.ml";
